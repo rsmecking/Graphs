2a053e51367641c86c7ef5945776d27f4e393f1c
Ryan Mecking <50974677+rsmecking@users.noreply.github.com>
2020-08-12 18:20:55 -0400

    Update ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -10,10 +10,6 @@ def earliest_ancestor(ancestors, starting_node):
     for vert in ancestors:
         graph.add_edge(vert[1], vert[0])
 
-    # print(graph.dft(starting_node))
-
-    # print(graph.vertices)
-
     earliest = graph.bft(starting_node)[-1]
     if earliest == starting_node:
         return -1
